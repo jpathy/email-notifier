f47f3df311f654ec3b1178893d9c58ae503eb73e
Jiten Pathy <jiten@lazycons.xyz>
2022-01-29 14:38:33 +0000

    terraform: fix thumbprint for github actions

--- aws/tf/actions_role.tf
+++ aws/tf/actions_role.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_openid_connect_provider" "github_oidc" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 data "aws_iam_policy_document" "actions_policy_data" {
